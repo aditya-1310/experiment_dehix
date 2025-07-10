75865703dc453f8acec9bbb721779e2d8d76a2cc
Aditya <bhadadeaditya1310@gmail.com>
2025-07-10 23:21:51 +0530

    working properly

--- dehix_fastify-main/src/clients/index.ts
+++ dehix_fastify-main/src/clients/index.ts
@@ -1,11 +1,19 @@
 import { FastifyInstance } from "fastify";
 // Remove incorrect import: import { firebaseClient } from "../common/services";
 import { MongoClient } from "./mongo.client";
+import { firebaseClient } from "../common/services/firebase.service";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const initializeClients = async (fastify: FastifyInstance) => {
   await MongoClient.init(fastify);
-  // Firebase initialization removed - not needed
+  // Initialize Firebase Admin SDK (required for authentication & other services)
+  try {
+    await firebaseClient.init();
+    fastify.log.info("Firebase Admin initialized successfully");
+  } catch (err) {
+    fastify.log.error({ err }, "Failed to initialize Firebase Admin SDK");
+    throw err;
+  }
 };
 
 export * from "./mongo.client";
